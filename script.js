9b7e1d4b49d13a290edf7b176847bfcd4b35ef0e
Ganeshbabu26 <ganeshbabumadhavan@gmail.com>
2025-06-21 19:45:55 +0530

    prime program created

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 document.write("babu");
+
+function isPrime(n) 
+{
+    if (n <= 1) return false;
+    for (let i = 2; i <= Math.sqrt(n); i++) 
+        {
+            if (n % i === 0) return false;
+        }
+    return true;
+}
